e3a7be2f0e0877d32774087ddc96df0ef175dac4
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-03 18:35:28 -0300

    test: ensure it can calculates dynamic price with multipliar 2X

--- tests/data/dynamicpricecalculator.service.spec.ts
+++ tests/data/dynamicpricecalculator.service.spec.ts
@@ -1,24 +1,11 @@
-import DynamicPriceCalculatorInterface, { DynamicPrice } from '@/domain/features/dynamicpricecalculator.interface'
+import DynamicPriceCalculatorService from '@/data/services/dynamicpricecalculator.service'
 
-class DynamicPriceCalculatorService implements DynamicPriceCalculatorInterface {
-  async calculate (params: DynamicPrice.Input): Promise<DynamicPrice.Output> {
-    console.log('[PARAMS]: ', params)
-    const priceCast = Number(params.price)
-    const currencyCast = Number(params.currency)
-    const multiplierCast = Number(params.multiplier)
-    const dynamicPrice = priceCast * (currencyCast * multiplierCast)
-    const local = dynamicPrice.toLocaleString('en-US', {
-      style: 'currency',
-      currency: params.localeKey
-    })
-    return {
-      pricein: local,
-      codein: params.localeKey,
-      ask: params.currency,
-      multiplier: `${params.multiplier}X`
-    }
-  }
-}
+const makeInputData = (multiplierParam: string): any => ({
+  price: '529.99',
+  currency: '0.1876',
+  multiplier: multiplierParam,
+  localeKey: 'USD'
+})
 
 describe('Dynamic price calculator', () => {
   let sut: DynamicPriceCalculatorService
@@ -28,18 +15,24 @@ describe('Dynamic price calculator', () => {
   })
 
   it('should calculate dynamic price with multipliar 1X', async () => {
-    const inputData = {
-      price: '529.99',
-      currency: '0.1876',
-      multiplier: '1',
-      localeKey: 'USD'
-    }
+    const inputData = makeInputData('1')
     const call = await sut.calculate(inputData)
     expect(call).toEqual({
       pricein: '$99.43',
       codein: inputData.localeKey,
       ask: inputData.currency,
       multiplier: '1X'
+    })
+  })
+
+  it('should calculate dynamic price with multipliar 2X', async () => {
+    const inputData = makeInputData('2')
+    const call = await sut.calculate(inputData)
+    expect(call).toEqual({
+      pricein: '$198.85',
+      codein: inputData.localeKey,
+      ask: inputData.currency,
+      multiplier: '2X'
     })
   })
 })
