3591432a8f518be52e75a59f85d13406d4601bbf
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-03 12:56:20 -0300

    refactor: complete DynamicPrice data

--- tests/domain/dynamicprice.entity.spec.ts
+++ tests/domain/dynamicprice.entity.spec.ts
@@ -1,11 +1,29 @@
 import DynamicPrice from '@/domain/entities/dynamicprice.entity'
+import { DataSources } from '@/domain/entities/datasources.enum.entity'
+
+const inputData = {
+  priceParam: '259.99',
+  codeParam: 'BRL',
+  in: [
+    {
+      pricein: '95.40',
+      codein: 'USD'
+    }
+  ],
+  source: {
+    source: DataSources.THIRDPARTY,
+    requestDate: '2022-07-02'
+  }
+}
 
 describe('Dynamic price entity', () => {
   it('should create a dynamic price entity with input data', async () => {
-    const sut = new DynamicPrice({ priceParam: '259.99', codeParam: 'BRL' })
+    const sut = new DynamicPrice(inputData)
     expect(sut).toEqual({
       price: '259.99',
-      code: 'BRL'
+      code: 'BRL',
+      in: inputData.in,
+      source: inputData.source
     })
   })
 })
