43b9e2409dae179e402d40df1e6ba7b723d71900
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-03 18:43:40 -0300

    test: ensure it can calculates a dynamic price if there are no multiplier in params

--- tests/data/dynamicpricecalculator.service.spec.ts
+++ tests/data/dynamicpricecalculator.service.spec.ts
@@ -1,6 +1,6 @@
 import DynamicPriceCalculatorService from '@/data/services/dynamicpricecalculator.service'
 
-const makeInputData = (multiplierParam: string): any => ({
+const makeInputData = (multiplierParam?: string): any => ({
   price: '529.99',
   currency: '0.1876',
   multiplier: multiplierParam,
@@ -33,6 +33,17 @@ describe('Dynamic price calculator', () => {
       codein: inputData.localeKey,
       ask: inputData.currency,
       multiplier: '2X'
+    })
+  })
+
+  it('should calculate dynamic price if there are no multiplier in params', async () => {
+    const inputData = makeInputData()
+    const call = await sut.calculate(inputData)
+    expect(call).toEqual({
+      pricein: '$99.43',
+      codein: inputData.localeKey,
+      ask: inputData.currency,
+      multiplier: '1X'
     })
   })
 })
