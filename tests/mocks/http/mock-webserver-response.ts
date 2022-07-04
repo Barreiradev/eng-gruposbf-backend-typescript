2542a5cf090213e525930cc758c0ff8e91ea8014
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-04 15:05:33 -0300

    chore: add GiveMeAValidWebServerResponse

---
+++ tests/mocks/http/mock-webserver-response.ts
@@ -0,0 +1,16 @@
+export const GiveMeAValidWebServerResponse = {
+  price: '529,99',
+  code: 'BRL',
+  in: [
+    {
+      pricein: '$99.40',
+      codein: 'USD',
+      ask: '0.1876',
+      multiplier: '1X'
+    }
+  ],
+  datasourceinfo: {
+    source: 'gruposbf-ds-a',
+    requestDate: '2022-07-03'
+  }
+}
