54d79d553604bd29680b20b9442d9a82b486b0d5
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-04 18:03:52 -0300

    feat: add body and params checker

--- src/infra/http/expressrouter.ts
+++ src/infra/http/expressrouter.ts
@@ -3,6 +3,14 @@ import { RequestHandler } from 'express'
 
 export const adaptExpressRoute = (controller: Controller): RequestHandler => {
   return async (request, response) => {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!(request.body.price) && request.params.price) {
+      request.body = {
+        price: request.params.price,
+        code: request.params.code,
+        codein: request.params.codein.split(',')
+      }
+    }
     const httpResponse = await controller.handle({ ...request.body })
     if (httpResponse.statusCode === 200) {
       response.status(httpResponse.statusCode).json(httpResponse.data)
