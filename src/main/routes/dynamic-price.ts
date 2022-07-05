81dae3033acdf088e7f0fbb40d7cccffc0105c29
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-04 22:21:07 -0300

    feat: add transformParamstoBody

--- src/main/routes/dynamic-price.ts
+++ src/main/routes/dynamic-price.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { adaptExpressRoute } from '@/infra/http/expressrouter'
+import { transformParamstoBody } from '@/infra/http/transformrequestparamstobody'
 import { makeDynamicPriceController } from '../factories/dynamic-price-controller'
 
 export default (router: Router): void => {
   const dynamicPriceController = makeDynamicPriceController()
-  router.get('/convert/price/:price/code/:code/codein/:codein', adaptExpressRoute(dynamicPriceController))
+  router.get('/convert/price/:price/code/:code/codein/:codein', transformParamstoBody, adaptExpressRoute(dynamicPriceController))
 }
