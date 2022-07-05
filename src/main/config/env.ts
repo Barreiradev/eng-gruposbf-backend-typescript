993a1673d8de64d6717d8e3708f7bd058213927b
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-05 14:13:37 -0300

    refactor: add env variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 require('dotenv/config')
 
 export const env = {
-  serverPort: process.env.APPLICATION_PORT ?? 3333,
+  serverPort: process.env.LOCAL_APPLICATION_PORT ?? process.env.PROD_APPLICATION_PORT ?? 3333,
   awesomeapiUrl: process.env.AWESOMEAPI_URL
 }
