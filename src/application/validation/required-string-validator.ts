fa4c7ae33995674cd0aecc380e3f1a52a157808e
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-05 21:34:35 -0300

    refactor: separate validation entities in single files

---
+++ src/application/validation/required-string-validator.ts
@@ -0,0 +1,19 @@
+import { RequiredFieldError } from '../errors/required-field-error'
+
+export class RequiredStringValidator {
+  constructor (
+    private readonly value: string,
+    private readonly fieldName: string
+  ) {}
+
+  validate (): Error | undefined {
+    if (
+      this.value === '' ||
+      this.value === null ||
+      this.value === undefined
+    ) {
+      return new RequiredFieldError(this.fieldName)
+    }
+    return undefined
+  }
+}
--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,51 +1,5 @@
-export class RequiredFieldError extends Error {
-  constructor (fieldName: string) {
-    super(`The field ${fieldName} is required`)
-  }
-}
-
-export class RequiredStringValidator {
-  constructor (
-    private readonly value: string,
-    private readonly fieldName: string
-  ) {}
-
-  validate (): Error | undefined {
-    if (
-      this.value === '' ||
-      this.value === null ||
-      this.value === undefined
-    ) {
-      return new RequiredFieldError(this.fieldName)
-    }
-    return undefined
-  }
-}
-
-export interface Validator {
-  validate: () => Error | undefined
-}
-
-export class ValidationBuilder {
-  private constructor (
-    private readonly value: string,
-    private readonly fieldName: string,
-    private readonly validators: Validator[] = []
-  ) {}
-
-  static of (params: {value: string, fieldName: string}): ValidationBuilder {
-    return new ValidationBuilder(params.value, params.fieldName)
-  }
-
-  required (): ValidationBuilder {
-    this.validators.push(new RequiredStringValidator(this.value, this.fieldName))
-    return this
-  }
-
-  build (): Validator[] {
-    return this.validators
-  }
-}
+import { RequiredStringValidator } from '@/application/validation/required-string-validator'
+import { ValidationBuilder } from '@/application/validation/validation-builder'
 
 describe('Validation builder', () => {
   it('should return a RequiredString validator', () => {
