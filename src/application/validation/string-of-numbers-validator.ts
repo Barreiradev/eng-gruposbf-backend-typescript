4a767d9eb0ef75592482b93a3d8791d19737e6c5
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-05 22:17:59 -0300

    refactor: move validation entities to single files

---
+++ src/application/validation/string-of-numbers-validator.ts
@@ -0,0 +1,17 @@
+import NotValidCharactersError from '../errors/not-characters-error'
+
+export default class StringOfNumberValidator {
+  constructor (
+    private readonly value: string,
+    private readonly fieldName: string) {}
+
+  validate (): Error | undefined {
+    const regex = /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/
+    const minLengthRegex = new RegExp(regex)
+    const executeRegex = minLengthRegex.test(this.value)
+    if (!executeRegex) {
+      return new NotValidCharactersError(this.fieldName, 'numbers')
+    }
+    return undefined
+  }
+}
--- tests/application/validation/string-of-numbers.spec.ts
+++ tests/application/validation/string-of-numbers.spec.ts
@@ -1,42 +1,23 @@
-export class NotValidCharactersError extends Error {
-  constructor (fieldName: string, allowedCharactersType: string) {
-    super(`Not valid characters in ${fieldName}, only ${allowedCharactersType} are allowed`)
-  }
-}
-
-export class NumerableStringValidator {
-  constructor (
-    private readonly value: string,
-    private readonly fieldName: string) {}
-
-  validate (): Error | undefined {
-    const regex = /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/
-    const minLengthRegex = new RegExp(regex)
-    const executeRegex = minLengthRegex.test(this.value)
-    if (!executeRegex) {
-      return new NotValidCharactersError(this.fieldName, 'numbers')
-    }
-    return undefined
-  }
-}
+import StringOfNumberValidator from '@/application/validation/string-of-numbers-validator'
+import NotValidCharactersError from '@/application/errors/not-characters-error'
 
 describe('NumerableStringValidator', () => {
   it('should return NotValidCharactersError if string has alphabetic characters', () => {
-    const sut = new NumerableStringValidator('not_valid_price', 'price')
+    const sut = new StringOfNumberValidator('not_valid_price', 'price')
 
     const error = sut.validate()
 
     expect(error).toEqual(new NotValidCharactersError('price', 'numbers'))
   })
   it('should return NotValidCharactersError if a not valid price is provided', () => {
-    const sut = new NumerableStringValidator('000000000000000', 'price')
+    const sut = new StringOfNumberValidator('000000000000000', 'price')
 
     const error = sut.validate()
 
     expect(error).toEqual(new NotValidCharactersError('price', 'numbers'))
   })
   it('should return empty if price is valid', () => {
-    const sut = new NumerableStringValidator('529.99', 'price')
+    const sut = new StringOfNumberValidator('529.99', 'price')
 
     const error = sut.validate()
 
