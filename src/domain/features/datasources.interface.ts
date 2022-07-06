0759a2a9e43a34dc22b48b2a48cdb853b7399730
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-06 14:04:36 -0300

    refactor: add Input type to DataSourceInterface usecase

--- src/domain/features/datasources.interface.ts
+++ src/domain/features/datasources.interface.ts
@@ -1,10 +1,18 @@
 import DataSourceInfo from '../entities/datasourceinfo.entity'
 
+type DataSourceOption = {
+  url?: string
+  httpMethod?: string
+}
+
 export default interface DataSourceInterface {
-  execute: () => Promise<DataSource.Output>
+  execute: (params: DataSource.Input) => Promise<DataSource.Output>
 }
 
 export namespace DataSource {
+  export type Input = {
+    dataSourceOptions: DataSourceOption
+  }
   export type Output = {
     data: any
     datasource: DataSourceInfo
