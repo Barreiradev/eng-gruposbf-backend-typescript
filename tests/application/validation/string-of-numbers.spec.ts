bf5c6b18e9de54786176401a6f2ee3b5843fdce9
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-05 22:14:10 -0300

    test: ensure it returns NotValidCharactersError if a not valid price is provided

--- tests/application/validation/string-of-numbers.spec.ts
+++ tests/application/validation/string-of-numbers.spec.ts
@@ -22,7 +22,14 @@ export class NumerableStringValidator {
 
 describe('NumerableStringValidator', () => {
   it('should return NotValidCharactersError if string has alphabetic characters', () => {
-    const sut = new NumerableStringValidator('not_valid_vatNumber', 'price')
+    const sut = new NumerableStringValidator('not_valid_price', 'price')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new NotValidCharactersError('price', 'numbers'))
+  })
+  it('should return NotValidCharactersError if a not valid price is provided', () => {
+    const sut = new NumerableStringValidator('000000000000000', 'price')
 
     const error = sut.validate()
 
