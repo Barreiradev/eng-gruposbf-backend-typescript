abeb82e4d4ff1645c7e4604288ddef86891eba16
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-05 23:54:28 -0300

    feat: add validator methods to base Controller

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -1,13 +1,29 @@
-import { HttpResponse, serverError } from '../helpers/http/http-helpers'
+import { HttpRequest } from '@/data/gateways/http-gateway.interface'
+import { badRequest, HttpResponse, serverError } from '../helpers/http/http-helpers'
+import ValidationComposite from '../validation/composite'
+import { Validator } from '../validation/validator'
 
 export default abstract class Controller {
   abstract perform (httpRequest: any): Promise<HttpResponse>
 
+  buildValidators (httpRequest: any): Validator[] {
+    return []
+  }
+
   async handle (httpRequest: any): Promise<HttpResponse> {
+    const error = this.validate(httpRequest)
+    if (error !== undefined) {
+      return badRequest(error)
+    }
     try {
       return await this.perform(httpRequest)
     } catch (error) {
       return serverError(error as Error)
     }
+  }
+
+  private validate (httpRequest: HttpRequest): Error | undefined {
+    const validators = this.buildValidators(httpRequest)
+    return new ValidationComposite(validators).validate()
   }
 }
