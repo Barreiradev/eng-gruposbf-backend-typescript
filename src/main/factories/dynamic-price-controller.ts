8afaeef3c6573056153a2f0fed22268c6b7d4171
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-06 18:53:11 -0300

    refactor: separate Controller and DataSource factories

--- src/main/factories/dynamic-price-controller.ts
+++ src/main/factories/dynamic-price-controller.ts
@@ -1,11 +1,11 @@
 import { DynamicPriceController } from '@/application/controllers/dynamic-price-controller'
 import DynamicPriceService from '@/data/services/dynamicprice.service'
 import DynamicPriceCalculatorService from '@/data/services/dynamicpricecalculator.service'
-import AxiosHttpClient from '@/infra/http/axioshttpclient'
+import { makeDataSource } from './data-source'
 
 export const makeDynamicPriceController = (): DynamicPriceController => {
-  const httpClient = new AxiosHttpClient()
+  const dataSource = makeDataSource()
   const dynamicPriceCalculator = new DynamicPriceCalculatorService()
-  const dynamicPrice = new DynamicPriceService(httpClient, dynamicPriceCalculator)
+  const dynamicPrice = new DynamicPriceService(dataSource, dynamicPriceCalculator)
   return new DynamicPriceController(dynamicPrice)
 }
