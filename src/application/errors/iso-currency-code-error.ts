619863f15170a46dca949633ab94a8a912b0b7e7
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-05 22:55:58 -0300

    refactor: change ISOCurrencyCodeError message

--- src/application/errors/iso-currency-code-error.ts
+++ src/application/errors/iso-currency-code-error.ts
@@ -1,5 +1,5 @@
 export default class ISOCurrencyCodeError extends Error {
   constructor (fieldName: string) {
-    super(`The field ${fieldName} is too long`)
+    super(`The field ${fieldName} is not a ISO currency code`)
   }
 }
