76bfe96c2375171996be9add9743180da007c7b1
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-06 17:08:43 -0300

    refactor: change output to return a list of PgCurrency

--- src/data/repos/currency-data.interface.ts
+++ src/data/repos/currency-data.interface.ts
@@ -1,14 +1,9 @@
+import { PgCurrency } from '@/infra/postgres/entities/currency'
+
 export interface LoadCurrencyDataRepositoryInterface {
   load: () => Promise<LoadCurrencyDataRepository.Output>
 }
 
 export namespace LoadCurrencyDataRepository {
-  export type Output = null | undefined | {
-    id: string
-    code: string
-    codein: string
-    ask: string
-    create_date: string
-    request_date: string
-  }
+  export type Output = null | undefined | PgCurrency[]
 }
